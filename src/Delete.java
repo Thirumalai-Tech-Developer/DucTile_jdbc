f5161d1809f11ec168f3885966bf543f114b3348
Thirumalai-tech-Developer <thirutechdeveloper@gmail.com>
2024-08-19 12:26:46 -0400

    implement delete method

--- src/Delete.java
+++ src/Delete.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Delete {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Which Table did u want to delete??  " );
+            String a = sc.nextLine();
+            String sql = String.format("drop table %s", a);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
