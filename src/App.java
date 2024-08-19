08515f46dd87dac47cfb0d5632dcc551dd4955ea
Thirumalai-tech-Developer <thirutechdeveloper@gmail.com>
2024-08-19 13:42:47 -0400

    complete insert method

--- src/App.java
+++ src/App.java
@@ -1,11 +1,9 @@
 public class App {
+    public static final String URL = "jdbc:mysql://localhost:3306/gt";
+    public static final String USER = "root";
+    public static final String PASS = "gtking";
     public static void main(String[] args) {
         System.out.println("What operation did want perform \n 1.Create Table \n 2.Update Table \n 3.Insert Value in Table \n 4.Delete the Table");
         Create create = new Create();
-    }
-    public static void connectionDetails(){
-        String url = "jdbc:mysql://localhost:3306/gt";
-        String user = "root";
-        String pass = "gtking"; 
     }
 }
--- src/Insert.java
+++ src/Insert.java
@@ -1,11 +1,49 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Insert {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        App ap = new App();
         try {
             System.out.print("Enter tablename to insert value ???");
+            String tname = sc.nextLine();
+            String sl = String.format("insert into %s() ",tname);
+            sb.append(sl).append("value ");
+
+            Connection con = DriverManager.getConnection(ap.URL,ap.USER,ap.PASS);
+            Statement st = con.createStatement();
+            String sql = String.format("SELECT COUNT(*) AS total FROM %s",tname);
+            ResultSet rs = st.executeQuery(sql);
+            int count = 1;
+            while(rs.next()){
+                count++;
+            }
+            System.out.println("How many row's want to insert");
+            int n= sc.nextInt();
+            sc.nextLine();
+            for (int i = 1; i <= n; i++){
+                sb.append("(");
+                for(int j =1; j <= count ; j++){
+                    System.out.printf("column%d value ",j);
+                    sb.append("'").append(sc.nextLine()).append("'");
+                    if (j != count){
+                        sb.append(",");
+                    }
+                }
+                if ( i != n){
+                    sb.append("),");
+                }else sb.append(");");
+            }
+            System.out.println(sb.toString());
+            PreparedStatement pst = con.prepareStatement(sb.toString());
+            pst.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
