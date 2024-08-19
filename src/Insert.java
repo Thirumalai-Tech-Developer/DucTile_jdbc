bc0d0135daa193f7733b300a432ee1d40b004dbc
Thirumalai-tech-Developer <thirutechdeveloper@gmail.com>
2024-08-19 12:35:23 -0400

    pending insert

--- src/Insert.java
+++ src/Insert.java
@@ -1,3 +1,13 @@
+
+import java.util.Scanner;
+
 public class Insert {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter tablename to insert value ???");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
